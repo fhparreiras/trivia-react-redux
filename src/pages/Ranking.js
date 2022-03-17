d14698acb3af881c1a511cc145a847e2d305d51f
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-03-17 12:54:56 -0300

    Requisito 18 - com erro

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,11 +3,53 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Ranking extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sortedRanking: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const magicNum = (-1); // Ordenando o array em ordem decrescente:
+    ranking.sort((a, b) => { // https://www.horadecodar.com.br/2021/01/11/como-ordenar-um-array-de-objetos-em-javascript/;
+      if (a.score > b.score) {
+        return magicNum;
+      }
+      return true;
+    });
+    this.setState({ sortedRanking: ranking, isLoading: false });
+  }
+
   render() {
+    const { sortedRanking, isLoading } = this.state;
+    console.log('sortedRanking no render ===> ', sortedRanking);
     const { history } = this.props;
     return (
       <>
         <h1 data-testid="ranking-title">Ranking</h1>
+        {
+          isLoading
+            ? <h2>Carregando</h2>
+            : (
+              sortedRanking.map((player, index) => (
+                <div key={ index }>
+                  <img src={ player.picture } alt="gravatar-img" />
+                  <h3
+                    data-testid={ `player-name-${[index]}` }
+                  >
+                    {player.name}
+                  </h3>
+                  <h4
+                    data-testid={ `player-score-${[index]}` }
+                  >
+                    {player.score}
+                  </h4>
+                </div>
+              )))
+        }
         <button
           data-testid="btn-go-home"
           type="button"
